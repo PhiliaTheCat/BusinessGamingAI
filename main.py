d467259343c40170482e8fadd3a752137945e4a8
SkyWolfy682 <2787974051@qq.com>
2022-12-12 15:55:19 +0800

    Deepcopy Fixed

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from presetting import preset_company, preset_district
+from copy import deepcopy
 
 def attempt(company: list, label_c: int, district: list, label_d: int, depth: int) -> None:
-    # Special
-    if depth > 2: # This line controls the depth of iteration
+    # Process
+    company[label_c].add_new_branch()
+    district[label_d].add_new_branch(label_c)
+
+    # Sepcial Judge
+    if depth == 8: # This line controls the depth of iteration
         for i in range(2):
             cost: float = company[i].get_extra_cost_sum()
             sale: float = 0.0
@@ -11,18 +16,13 @@ def attempt(company: list, label_c: int, district: list, label_d: int, depth: in
                 sale += district[j].get_virtual_sale(i)
             print(sale - cost)
         return
-
-    # Process
-    company[label_c].add_new_branch()
-    district[label_d].add_new_branch(label_c)
-
-    # Next Level
-    for i in range(10): # Related to map size
-        attempt(company[: ], 1 - label_c, district[: ], i, depth + 1)
+    else:
+        for i in range(10): # Related to map size
+            attempt(deepcopy(company), 1 - label_c, deepcopy(district), i, depth + 1)
 
 company = preset_company()
 district = preset_district()
 
 for i in range(10):
-    attempt(company[: ], 0, district[: ], i, 1)
+    attempt(deepcopy(company), 0, deepcopy(district), i, 1)
     
