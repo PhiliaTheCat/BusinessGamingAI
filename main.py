6797381d66bce37d46bec33cf26da7d6fcaca1d6
SkyWolfy682 <2787974051@qq.com>
2022-12-13 11:38:50 +0800

    Tiny Change

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ def attempt(company: list, label_c: int, district: list, label_d: int, depth: in
     district[label_d].add_new_branch(label_c)
 
     # Sepcial Judge
-    if depth == 8: # This line controls the depth of iteration
-        for i in range(2):
-            cost: float = company[i].get_extra_cost_sum()
-            sale: float = 0.0
-            for j in range(10):
-                cost += district[j].get_basic_cost_sum(i)
-                sale += district[j].get_virtual_sale(i)
-            print(sale - cost)
-        return
+    if depth == 4: # This line controls the depth of iteration
+        cost_a: float = company[0].get_extra_cost_sum()
+        cost_b: float = company[1].get_extra_cost_sum();
+        sale_a: float = 0.0
+        sale_b: float = 0.0
+        for i in range(10): # Related to map size
+            cost_a += district[i].get_basic_cost_sum(0)
+            cost_b += district[i].get_basic_cost_sum(1)
+            sale_a += district[i].get_virtual_sale(0)
+            sale_b += district[i].get_virtual_sale(1)
+        print(f"{sale_a - cost_a}, {sale_b - cost_b}")
     else:
         for i in range(10): # Related to map size
             attempt(deepcopy(company), 1 - label_c, deepcopy(district), i, depth + 1)
